52ef8bded741887581979d33274b292d463443bb
StormBT89 <glavevski.pece@gmail.com>
2025-04-12 20:59:35 +0200

    create signup api route

--- api/index.js
+++ api/index.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import moongoose from 'mongoose';
 import dotenv from 'dotenv';
-import userRoutes from './routes/user.route.js'
+import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
 
 dotenv.config();
 
@@ -13,11 +14,14 @@ moongoose.connect(process.env.MONGO).then(
 })
 const app = express();
 
+app.use(express.json());
+
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
 
 app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
 
 
 
