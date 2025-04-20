f1e8fd267660f5aee3d4878bc9578a74c1e85303
StormBT89 <glavevski.pece@gmail.com>
2025-04-20 06:47:43 +0200

    add update post functionality

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -93,4 +93,25 @@ export const deletepost = async (req, res, next) => {
     }
 }
 
+export const updatepost = async (req, res, next) => {
+    if (!req.user.isAdmin || req.user.id !== req.params.userId) {
+        return next(errorHandler(403, 'Немате привилегии за да промените податоци за состанокот'));
+    }
+
+    try {
+        const updatedPost = await Post.findByIdAndUpdate(
+            req.params.postId, {
+                $set: {
+                    title: req.body.title,
+                    content: req.body.content,
+                    category: req.body.category,
+                    image: req.body.image,
+                }, 
+        }, {new: true}
+        );
+        res.status(200).json(updatedPost);
+    } catch (error) {
+        next(error);        
+    }
+}
 
---
+++ client/src/pages/UpdatePost.jsx
@@ -0,0 +1,159 @@
+import { TextInput, Select, FileInput, Button, Textarea, Alert } from 'flowbite-react'
+import { useEffect, useState } from 'react'
+import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage'
+import { app } from '../firebase.js'
+import { CircularProgressbar } from 'react-circular-progressbar'
+import 'react-circular-progressbar/dist/styles.css'
+import {useNavigate, useParams} from 'react-router-dom'
+import {useSelector } from 'react-redux'
+
+export default function UpdatePost() {
+  const [file, setFile] = useState(null);
+  const [imageUploadProgress, setImageUploadProgress] = useState(null);
+  const [imageUploadError, setImageUploadError] = useState(null);
+  const [formData, setFormData] = useState({});
+  const [publishError, setPublishError] = useState(null);
+  const { postId } = useParams();
+
+  const navigate = useNavigate();
+  const {currentUser} = useSelector((state) => state.user);
+
+  useEffect(() => {
+    try {
+
+        const fetchPost = async () => {
+        const res = await fetch(`/api/post/getposts?postId=${postId}`);
+        const data = await res.json();
+        if (!res.ok) {
+            console.log(data.message);
+            setPublishError(data.message);
+            return;
+        }
+        if (res.ok) {
+            setPublishError(null);
+            setFormData(data.posts[0]);
+        }
+        }        
+        fetchPost();        
+    } 
+    catch (error) {
+        console.log(error,message);        
+    }
+
+  }, [postId]);
+  
+
+  const handleUploadImage = async () => {
+    try {
+      if(!file) {
+        setImageUploadError('Ве молиме изберете фотографија за прикачување');
+        return;
+      } 
+      setImageUploadError(null);
+      const storage = getStorage(app);
+      const fileName = new Date().getTime() + '-' + file.name;
+      const storageRef = ref(storage, fileName);
+      const uploadTask = uploadBytesResumable(storageRef, file);
+
+      uploadTask.on(
+        'state_changed',
+        (snapshot) => {
+          const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          setImageUploadProgress(progress.toFixed(0));
+        },
+        (error) => {
+          setImageUploadError('Грешка во прикачувањето на фотографијата');
+          setImageUploadProgress(null);
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadUrl) => {
+            setImageUploadProgress(null);
+            setImageUploadError(null);
+            setFormData({...formData, image: downloadUrl});
+          });
+        }
+      );  
+    } catch (error) {
+      setImageUploadError('Прикачувањето на фотографијата е неуспешно');
+      setImageUploadProgress(null);
+      console.log(error);
+    }
+
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const res = await fetch(`/api/post/updatepost/${formData._id}/${currentUser._id}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (!res.ok) {
+        setPublishError(data.message);
+        return;
+      } 
+      if (res.ok) {
+        setPublishError(null);
+        navigate(`/post/${data.slug}`);
+      }        
+    } catch (error) {
+      setPublishError('Постојат проблеми со ажурирањето на состанокот');      
+    }
+  }
+
+  return (
+    <div className="p-3 max-w-3xl mx-auto min-h-screen">
+      <h1 className="text-center text-3xl my=7 font-semibold mb-4">Ажурирање на податоци за состанокот</h1>        
+      <form className="flex flex-col gap-4" onSubmit={handleSubmit} >
+        <div className="flex flex-col gap-4 sm:flex-row justify-between">
+          <TextInput type='text' id='title' placeholder='Наслов на состанокот..' required className='flex-1'
+                     value={formData.title} onChange={(e) => setFormData({...formData, title: e.target.value})}  />
+            <Select value={formData.category} onChange={(e) => setFormData({...formData, category: e.target.value})} >
+              <option value='kolegijalen'>Колегиум</option>
+              <option value='nedelen'>Неделен</option>
+              <option value='iten'>Итен</option>
+              <option value='mesecen'>Месечен</option>
+            </Select>
+        </div>
+        <div className='flex gap-4 items-center justify-between border-4 border-teal-500 border-dotted p-3'>
+          <FileInput type='file' accept='image/*' onChange={(e) => setFile(e.target.files[0])}/>
+          <Button disabled={imageUploadProgress} type='button' onClick={handleUploadImage} size='sm' className='bg-gradient-to-r from-white-200 via-gray-500 to-black-500'>
+            {
+              imageUploadProgress ?
+              (<div className='w-16 h-16'>
+                <CircularProgressbar value={imageUploadProgress} text={`${imageUploadProgress || 0}%`}/>                                
+              </div>) : (
+                  'Прикачи фотографија'
+              ) 
+            }            
+          </Button>
+        </div>
+        {
+          imageUploadError && (
+           <Alert color='failure'>
+            {imageUploadError}
+           </Alert>
+          )
+        }
+        {
+          formData.image && (
+            <img src={formData.image} alt='прикачена фотографија' className='w-full h-72 object-cover'/>
+          )
+        }
+        <Textarea value={formData.content} onChange={(e) => setFormData({...formData, content: e.target.value})} id='content' placeholder='Инфо за состанок..' required rows={5}>
+        </Textarea>
+        <Button type='submit' className='bg-gradient-to-r from-white-200 via-gray-500 to-black-500'>
+          Ажурирај состанок
+        </Button>
+        {
+          publishError && <Alert className='mt-5' color='failure'>{publishError}</Alert>
+        }
+      </form>         
+    </div>
+  )
+}
