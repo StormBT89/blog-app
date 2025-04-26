c806d73c74f8db2806709c70adb321592774f4a1
StormBT89 <glavevski.pece@gmail.com>
2025-04-26 08:38:05 +0200

    add recent article section to the post page

--- client/src/components/Comment.jsx
+++ client/src/components/Comment.jsx
@@ -84,14 +84,10 @@ export default function Comment({comment, onLike, onEdit, onDelete}) {
             </>            
           ) : (
             <>
-
-
-            </>
-          )
-        }
-        <p className="text-gray-500 pb-2">
-          {comment.content}
-          <div className="flex items-center pt-2 text-xs gap-2 max-w-fit">
+            <p className="text-gray-500 pb-2">
+                 {comment.content}
+            </p>
+            <div className="flex items-center pt-2 text-xs gap-2 max-w-fit">
             <button type="button" onClick={() => onLike(comment._id)} className={`text-gray-400 hover:text-blue-500 ${
               currentUser && comment.likes.includes(currentUser._id) && '!text-blue-500'
             }`}>
@@ -112,12 +108,13 @@ export default function Comment({comment, onLike, onEdit, onDelete}) {
                  <button type="button" onClick={() => onDelete(comment._id)} className="text-gray-400 hover:text-red-500 cursor-pointer">
                  Избриши 
                </button>
-                </>
-                
+                </>                
               )
             }
-          </div>
-        </p>
+          </div>  
+            </>
+          )
+        }                         
       </div>
     </div>
   )
